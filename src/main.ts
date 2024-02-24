5bd310f7b8d0f98ff06e6347779476fce4b4e68b
IYarovoy <IYarovoy@korusconsulting.ru>
2024-02-24 20:08:24 +0300

    add exeption filter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { App } from "./app.js";
 import { LoggerService } from "./logger/logger.service";
 import { UsersController } from "./users/users.controller";
+import { ExeptionFilter } from "./errors/exeption.filter";
 
 async function bootstrap() {
     const logger = new LoggerService();
     const userController = new UsersController(logger);
-    const app = new App(logger, userController);
+    const exeptionFilter = new ExeptionFilter(logger);
+    const app = new App(logger, userController, exeptionFilter);
     await app.init();
 };
 
