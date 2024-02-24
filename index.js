65351d0522f4658fda58326a1a6b8ebc7c4dbef7
IYarovoy <IYarovoy@korusconsulting.ru>
2024-02-24 16:46:08 +0300

    add ts

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-import express from "express";
-import { userRouter } from "./users/users.js";
-
-const port = 8000;
-const app = express();
-
-app.use((req, res, next) => {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var users_js_1 = require("./users/users.js");
+var port = 8000;
+var app = (0, express_1.default)();
+app.use(function (req, res, next) {
     console.log("Time:", Date.now());
     next();
 });
-
-app.get("/hello", (req, res) => {
-    res.send( {success: true} );
+app.get("/hello", function (req, res) {
+    res.send({ success: true });
 });
-
-app.use("/users", userRouter);
-
-app.use((err,req, res, next) => {
+app.use("/users", users_js_1.userRouter);
+app.use(function (err, req, res, next) {
     console.log(err.message);
     res.status(500).send(err.message);
 });
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+app.listen(port, function () {
+    console.log("Server running at http://localhost:".concat(port, "/"));
 });
--- users/users.js
+++ users/users.js
@@ -1,18 +1,16 @@
-import express from "express";
-
-const userRouter = express.Router();
-
-userRouter.use((req, res, next) => {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.userRouter = void 0;
+var express_1 = require("express");
+var userRouter = express_1.default.Router();
+exports.userRouter = userRouter;
+userRouter.use(function (req, res, next) {
     console.log("userRouter Time:", Date.now());
     next();
-})
-
-userRouter.post("/login", (req, res) => {
-    res.send( "login" );
 });
-
-userRouter.post("/register", (req, res) => {
-    res.send( "register" );
+userRouter.post("/login", function (req, res) {
+    res.send("login");
 });
-
-export { userRouter };
+userRouter.post("/register", function (req, res) {
+    res.send("register");
+});
