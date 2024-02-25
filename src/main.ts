5958e8dcef88acd48cc1db479d09da44153737a8
IYarovoy <IYarovoy@korusconsulting.ru>
2024-02-25 18:26:02 +0300

    add DI

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,25 @@ import { App } from "./app.js";
 import { LoggerService } from "./logger/logger.service";
 import { UsersController } from "./users/users.controller";
 import { ExeptionFilter } from "./errors/exeption.filter";
+import { Container, ContainerModule, interfaces } from "inversify";
+import { ILogger } from "./logger/logger.interface";
+import { TYPES } from "./types";
+import { IExceptionFilter } from "./errors/exeption.filter.interface";
+import { IUsersInterface } from "./users/users.interface";
 
-async function bootstrap() {
-    const logger = new LoggerService();
-    const userController = new UsersController(logger);
-    const exeptionFilter = new ExeptionFilter(logger);
-    const app = new App(logger, userController, exeptionFilter);
-    await app.init();
-};
+export const appBindings = new ContainerModule((bind:interfaces.Bind) => {
+   bind<ILogger>(TYPES.ILogger).to(LoggerService);
+   bind<IExceptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
+   bind<IUsersInterface>(TYPES.UserController).to(UsersController);
+   bind<App>(TYPES.Application).to(App);
+});
 
-bootstrap();
+function bootstrap() {
+   const appContainer = new Container();
+   appContainer.load(appBindings);
+   const app = appContainer.get<App>(TYPES.Application);
+   app.init();
+   return { app, appContainer };
+}
+
+export const { app, appContainer } = bootstrap()
